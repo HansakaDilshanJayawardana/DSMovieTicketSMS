d0b323a4a90d29a2dd8dd22ecc2ffd629bafda42
HansakaDilshanJayawardana <hansaka345@gmail.com>
2022-07-06 10:00:03 +0530

    Initial Commit

---
+++ src/main/java/com/sms/dsmovieticketsms/DsMovieTicketSmsApplication.java
@@ -0,0 +1,19 @@
+package com.sms.dsmovieticketsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@SpringBootApplication
+public class DsMovieTicketSmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsMovieTicketSmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sms/dsmovieticketsms/DsMovieTicketSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sms.dsmovieticketsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMovieTicketSmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
